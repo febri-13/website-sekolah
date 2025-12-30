bdce63d6e93fda80c00e239fad89c045273e21fa
gameCahya <gamecahya13@gmail.com>
2025-12-30 14:12:27 +0700

    Update Halaman Berita

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  }),
   integrations: [
     tailwind({
       configFile: './tailwind.config.cjs',

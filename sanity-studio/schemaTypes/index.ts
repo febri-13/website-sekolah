9fda255bdf3733aee87454287582e8aff02a96bc
HafizDaffa01 <hafizdaffaw@gmail.com>
2026-05-15 21:48:58 +0700

    style: revamping style in route /the_tinkers

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -1,9 +1,19 @@
 import stats from './stats'
 import features from './features'
 import berita from './berita'
+import galeri from './galeri'
+import pengumuman from './pengumuman'
+import program from './program'
+import guru from './guru'
+import teacher from './teacher'
 
 export const schemaTypes = [
     stats,
     features,
-    berita
+    berita,
+    galeri,
+    pengumuman,
+    program,
+    guru,
+    teacher
 ]
